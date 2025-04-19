d096facdbae44f35c4111264e924b31daa152e7d
Haniya <haniyasajjad18@gmail.com>
2025-04-19 16:18:32 +0500

    Create iconMap.js

---
+++ src/NGO/utilis/iconMap.js
@@ -0,0 +1,17 @@
+import {
+  Dashboard as DashboardIcon,
+  Add as AddIcon,
+  People as PeopleIcon,
+  Event as EventIcon,
+  Notifications as NotificationsIcon,
+  Message as MessageIcon,
+} from '@mui/icons-material';
+
+export const iconMap = {
+  DashboardIcon,
+  AddIcon,
+  PeopleIcon,
+  EventIcon,
+  NotificationsIcon,
+  MessageIcon,
+}; 
